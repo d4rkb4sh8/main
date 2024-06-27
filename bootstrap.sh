b8a93831cda2d004fee3fb9cab330c791a66bd7e
d4rkb4sh <d4rkb4sh@host>
2024-06-27 01:06:30 -0600

    updated for debian

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,32 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+
+# Remove Debian games bloatware
+sudo apt purge gnome-games libreoffice* -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+# Update system and install necessary tools
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+# Define APT packages
+APT_PACKAGES="fastfetch stow figlet lynis gawk curl wget git alacritty powerline* nala net-tools forensics-all cpufetch btop gnome-shell-extension-manager flatpak gnome-software-plugin-flatpak gh lolcat fd-find sd npm vlc build-essential procps file net-tools httpie mitmproxy gpaste-2 font-manager gdebi ufw gawk cmake plocate bat most libssl-dev libvips-dev libsixel-dev libchafa-dev libtbb-dev"
+
+# Function to install APT packages
+install_apt_packages() {
+    sudo apt install -y $APT_PACKAGES
+}
+# Call function to install APT packages
+install_apt_packages
+
+
+#install starship
+curl -sS https://starship.rs/install.sh | sh
+
+# add to bashrc
+eval "$(starship init bash)" >> $HOME/.bashrc
+
+# add  Preset
+starship preset nerd-font-symbols -o ~/.config/starship.toml
+
 
 # Install Homebrew
 install_homebrew() {
@@ -8,7 +36,7 @@ install_homebrew() {
 install_homebrew
 
 # Define Homebrew packages
-HOMEBREW_PACKAGES="gcc dust dog eza zellij neovim xh yazi unar jq poppler ripgrep zoxide tlrc"
+HOMEBREW_PACKAGES="gcc dust dog eza zellij neovim xh yazi ffmpegthumbnailer unar jq poppler fd ripgrep zoxide tlrc"
 
 # Function to install Homebrew packages
 install_homebrew_packages() {
@@ -17,6 +45,10 @@ install_homebrew_packages() {
 # Call function to install Homebrew packages
 install_homebrew_packages
 
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" >> .bashrc
+
+
 # Install ble.sh
 install_ble_sh() {
     # NOTE: It's recommended to download and inspect scripts before execution
@@ -24,23 +56,18 @@ install_ble_sh() {
     make -C ble.sh install PREFIX=~/.local &&
     echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
 }
-
+# Call function to install ble.sh
 install_ble_sh
 
-# Pull git repo
-install_repo() {
-    git clone https://github.com/st3f4nh4ck/.dotfiles.git $HOME/.dotfiles &&
-    rm -rf $HOME/.bashrc &&
-    stow $HOME/.dotfiles
-}
-# Call function to pull git repo
-install_repo
+# install flatpak
+sudo apt install flatpak
+sudo apt install gnome-software-plugin-flatpak
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 # Install GDM Settings
 install_gdm_settings() {
     # Install Flatpak applications
-   sudo flatpak install io.github.realmazharhussain.GdmSettings org.onlyoffice.desktopeditors org.gnome.World.PikaBackup io.gitlab.zehkira.Monophony io.github.mimbrero.WhatsAppDesktop org.chromium.Chromium org.gnome.Solanum org.qbittorrent.qBittorrent io.github.hvdwofl.jExifToolGUI com.belmoussaoui.Obfuscate net.sourceforge.Pdfedit
-
+    sudo flatpak install -y io.github.realmazharhussain.GdmSettings org.onlyoffice.desktopeditors org.gnome.World.PikaBackup io.gitlab.zehkira.Monophony io.github.mimbrero.WhatsAppDesktop org.chromium.Chromium org.gnome.Solanum org.qbittorrent.qBittorrent io.github.hvdwofl.jExifToolGUI com.belmoussaoui.Obfuscate net.sourceforge.Pdfedit
 }
 # Call function to install GDM Settings
 install_gdm_settings
@@ -55,14 +82,14 @@ install_snap_store() {
 # Call function to install Snap Store
 install_snap_store
 
-# Function to install VirtualBox
+# Install VirtualBox
 install_virtualbox() {
-        wget -O- -q https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg
-        echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http://download.virtualbox.org/virtualbox/debian bookworm contrib" |sudo tee /etc/apt/sources.list.d/virtualbox.list
-        sudo apt update
-        sudo apt install virtualbox-7.0 -y
+    wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmour -o /usr/share/keyrings/oracle_vbox_2016.gpg &&
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/oracle_vbox_2016.gpg] http://download.virtualbox.org/virtualbox/debian bookworm contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list &&
+    sudo apt update &&
+    sudo apt install virtualbox-7.0 -y
 }
-# Call function install virtualbox
+# Call function to install VirtualBox
 install_virtualbox
 
 # Install VBox Guest Extension
@@ -99,11 +126,16 @@ echo 'export PATH=$PATH:/opt' >> $HOME/.bashrc
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-# Remove Debian games bloatware
-sudo apt purge gnome-games libreoffice* -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+#rust installed
+. "$HOME/.cargo/env" >> .bahrc
 
 # Upgrade system with nala
 sudo nala upgrade
+
+# Update system and install necessary tools
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
 
 # Display message
 figlet "The Machine is Ready."
