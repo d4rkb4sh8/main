b38fab50ae08ec8d1b3e8cba950e2dc97a615c5c
h4ck3r <h4ck3r@khost>
2024-07-21 15:52:50 -0600

    added bash scripts

---
+++ programming/bash/scripts/hw.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+#Title        :hw
+#Date         :2024-07-21
+#Author       :h4ck3r
+#Version      :1
+#Description  :print Hello World!
+#Options      :None
+
+printf "%s\n" "Hello World !"
