ea64e960774ab26ad91ac571c54870fb34416ee4
h4ck3r <h4ck3r@dhost>
2024-08-14 21:00:24 -0600

    updated

--- bootstrap_debian.sh
+++ bootstrap_debian.sh
@@ -6,6 +6,20 @@ log() {
     echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')] $*"
 }
 
+# Append 'contrib non-free' to each active deb repository line in sources.list
+log "Adding 'contrib non-free' to active lines in sources.list..."
+sudo sed -i '/^deb / s/$/ contrib non-free/' /etc/apt/sources.list
+
+# Edit /etc/default/grub for GRUB settings
+log "Configuring GRUB settings..."
+sudo sed -i 's/^GRUB_TIMEOUT=.*/GRUB_TIMEOUT=2/' /etc/default/grub
+sudo sed -i '/^GRUB_CMDLINE_LINUX_DEFAULT=/a GRUB_CMDLINE_LINUX="rhgb quiet mitigations=off"' /etc/default/grub
+
+# Update GRUB and initramfs
+log "Updating GRUB and initramfs..."
+sudo update-grub
+sudo update-initramfs -u -k all
+
 # Remove Debian games bloatware and clean up
 log "Removing bloatware and cleaning up..."
 sudo apt purge -y gnome-games libreoffice* && sudo apt autoremove -y && sudo apt autoclean -y
@@ -16,18 +30,18 @@ sudo apt update -y && sudo apt full-upgrade -y && sudo apt autoremove -y && sudo
 
 # Define APT packages
 APT_PACKAGES=(
-   debian-goodies dict wikipedia2text w3m neovim netdiscover gpg pass zathura pipx python3-websocket wmctrl python3-levenshtein stow figlet lynis gawk curl wget git
-    tilix fd-find powerline* nala net-tools forensics-all forensics-full cpufetch btop gnome-shell-extension-manager flatpak gnome-software-plugin-flatpak 
-    gh lolcat fd-find sd npm vlc build-essential procps tldr file fzf ytfzf net-tools httpie mitmproxy gpaste-2 dkms 
-    font-manager gdebi ufw gawk cmake plocate bat most libssl-dev libvips-dev libsixel-dev libchafa-dev libtbb-dev ufw gdebi 
-    dconf-cli uuid-runtime linux-headers-$(uname -r) gpgv2 autoconf bison build-essential postgresql libaprutil1 
-    libgmp3-dev libpcap-dev openssl libpq-dev libreadline6-dev libsqlite3-dev libssl-dev locate libsvn1 libtool libxml2 libxml2-dev 
-    libxslt-dev wget libyaml-dev ncurses-dev  postgresql-contrib xsel zlib1g zlib1g-dev curl wireshark aircrack-ng macchanger sqlmap -y
+    debian-goodies dict wikipedia2text w3m neovim netdiscover gpg pass zathura pipx python3-websocket wmctrl python3-levenshtein stow figlet lynis gawk curl wget git
+    tilix fd-find powerline nala net-tools forensics-all forensics-full cpufetch btop gnome-shell-extension-manager flatpak gnome-software-plugin-flatpak
+    gh lolcat fd-find sd npm vlc build-essential procps tldr file fzf ytfzf net-tools httpie mitmproxy gpaste-2 dkms
+    font-manager gdebi ufw gawk cmake plocate bat most libssl-dev libvips-dev libsixel-dev libchafa-dev libtbb-dev ufw gdebi
+    dconf-cli uuid-runtime linux-headers-$(uname -r) gpgv2 autoconf bison build-essential postgresql libaprutil1
+    libgmp3-dev libpcap-dev openssl libpq-dev libreadline6-dev libsqlite3-dev libssl-dev locate libsvn1 libtool libxml2 libxml2-dev
+    libxslt-dev wget libyaml-dev ncurses-dev postgresql-contrib xsel zlib1g zlib1g-dev curl wireshark aircrack-ng macchanger sqlmap
 )
 
 # Install APT packages
 log "Installing APT packages..."
-sudo apt install -y "${APT_PACKAGES[@]}"
+sudo apt update -y && sudo apt install -y "${APT_PACKAGES[@]}"
 
 # Clone repo
 log "Cloning repository..."
@@ -41,83 +55,9 @@ echo 'export PATH=$PATH:/opt:/usr/local/bin' >> ~/.bashrc
 log "Installing Starship..."
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
-starship preset nerd-font-symbols -o ~/.config/starship.toml
 
-# Install Homebrew
-log "Installing Homebrew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-# Define Homebrew packages
-HOMEBREW_PACKAGES=(
-     dust xh yazi   
-)
-
-# Install Homebrew packages
-log "Installing Homebrew packages..."
-brew install "${HOMEBREW_PACKAGES[@]}"
-
-# Install custom scripts and configurations
-log "Setting up custom scripts and configurations..."
-cp -r "$HOME/gitprojects/main/ulauncher" "$HOME/.config/"
-
-# Install tgpt
-log "Installing tgpt..."
-curl -sSL https://raw.githubusercontent.com/aandrew-me/tgpt/main/install | bash -s /usr/local/bin
-
-# Install Rust
-log "Installing Rust..."
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-echo 'source "$HOME/.cargo/env"' >> ~/.bashrc
-source "$HOME/.cargo/env"
-
-# Install LazyVim
-log "Installing LazyVim..."
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
-
-# Copy bash aliases
-log "Copying bash aliases..."
-cp "$HOME/gitprojects/main/.bash_aliases" "$HOME/"
-
-# Copy nanorc
-log "Copying nanorc..."
-sudo cp "$HOME/gitprojects/main/nanorc" /etc/
-
-#install atuin
-curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh
-
-#install lazygit
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-
-#install Metasploit
-cd /opt ; sudo curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall
-chmod 755 msfinstall ; ./msfinstall
-
-#install realtek alfa adaptor
-sudo git clone -b v5.6.4.2 https://github.com/aircrack-ng/rtl8812au.git
-cd rtl* ; sudo make dkms_install
-cd $HOME
-
-# Setup UFW
-log "Setting up UFW..."
-sudo ufw limit 22/tcp
-sudo ufw allow 80/tcp
-sudo ufw allow 443/tcp
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-sudo ufw enable
-
-# Install GTFOBLookup
-log "Installing GTFOBLookup..."
-pipx install git+https://github.com/nccgroup/GTFOBLookup.git
-
-# Install Flathub
-log "Installing Flathub..."
+# Setup Flatpak and add Flathub
+log "Setting up Flatpak and adding Flathub..."
 sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install Snap
@@ -126,32 +66,36 @@ sudo apt install -y snapd
 sudo systemctl enable --now snapd apparmor
 sudo ln -s /var/lib/snapd/snap /snap
 
-# Install VirtualBox
+# Install VirtualBox from the official website
 log "Installing VirtualBox..."
-wget https://download.virtualbox.org/virtualbox/7.0.18/virtualbox-7.0_7.0.18-162988~Debian~bookworm_amd64.deb -O $HOME/Downloads/virtualbox.deb
-wget https://download.virtualbox.org/virtualbox/7.0.18/Oracle_VM_VirtualBox_Extension_Pack-7.0.18.vbox-extpack -O $HOME/Downloads/virtualbox-extpack.vbox-extpack
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/oracle_vbox_2016.gpg
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+sudo apt update
+sudo apt install -y virtualbox-7.0
+
+# Download and install VirtualBox Extension Pack
+log "Installing VirtualBox Extension Pack..."
+VBOX_VERSION=$(vboxmanage -v | cut -dr -f1)
+wget https://download.virtualbox.org/virtualbox/$VBOX_VERSION/Oracle_VM_VirtualBox_Extension_Pack-$VBOX_VERSION.vbox-extpack -O $HOME/Downloads/virtualbox-extpack.vbox-extpack
+sudo VBoxManage extpack install $HOME/Downloads/virtualbox-extpack.vbox-extpack --replace
 
 # Install Gogh terminal profile theme
 log "Installing Gogh terminal profile theme..."
 bash -c "$(wget -qO- https://git.io/vQgMr)"
 
-#Install ble.sh
+# Install ble.sh
 cd $HOME
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
 make -C ble.sh install PREFIX=~/.local
 echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
 
-# removing other terminal emulators
+# Remove other terminal emulators and set Tilix as default
 sudo apt purge gnome-terminal xterm -y
-
-#set tilix as default gnome terminal
 gsettings set org.gnome.desktop.default-applications.terminal exec /usr/bin/tilix.wrapper
 gsettings set org.gnome.desktop.default-applications.terminal exec-arg "-x"
+sudo update-alternatives --set x-terminal-emulator /usr/bin/tilix.wrapper
 
-#config tilix as default 2nd step
-sudo update-alternatives --config x-terminal-emulator
-
-#install Eza
+# Install Eza
 sudo mkdir -p /etc/apt/keyrings
 wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
 echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
@@ -159,10 +103,17 @@ sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.lis
 sudo apt update
 sudo apt install -y eza
 
-
 # Final update and clean up
 log "Final update and clean up..."
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt autoclean -y
+
+# Add keyboard shortcuts for brightness control to .bashrc
+log "Adding keyboard shortcuts for brightness control..."
+echo "
+# Brightness control from keyboard
+gsettings set org.gnome.settings-daemon.plugins.media-keys screen-brightness-up \"['<Ctrl><Super>Up']\"
+gsettings set org.gnome.settings-daemon.plugins.media-keys screen-brightness-down \"['<Ctrl><Super>Down']\"
+" >> ~/.bashrc
 
 # Source .bashrc
 source $HOME/.bashrc
